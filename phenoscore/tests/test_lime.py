0d76d1192c5819ac0efa635d0bb269cec3ec7caf
Lex Dingemans <a.dingemans@radboudumc.nl>
2022-10-12 11:38:50 -0400

    added except for github tests

--- phenoscore/tests/test_lime.py
+++ phenoscore/tests/test_lime.py
@@ -19,6 +19,11 @@ class CrossValLIMETester(unittest.TestCase):
             X, y, img_paths, df_data = self._phenoscorer.load_data_from_excel(os.path.join("phenoscore", "sample_data",
                                                                                        "satb1_data.xlsx"))
         self._phenoscorer.get_lime(X, y, img_paths, n_lime=1)
-        self._phenoscorer.gen_lime_and_results_figure(bg_image=os.path.join("..", "sample_data",
-                                                                            "background_image.jpg"),
-                                                      df_data=df_data, filename='lime_figure_' + 'SATB1_test.pdf')
+        try:
+            self._phenoscorer.gen_lime_and_results_figure(bg_image=os.path.join("..", "sample_data",
+                                                                                "background_image.jpg"),
+                                                          df_data=df_data, filename='lime_figure_' + 'SATB1_test.pdf')
+        except:
+            self._phenoscorer.gen_lime_and_results_figure(bg_image=os.path.join("phenoscore", "sample_data",
+                                                                                "background_image.jpg"),
+                                                          df_data=df_data, filename='lime_figure_' + 'SATB1_test.pdf')
