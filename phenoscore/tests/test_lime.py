d48a4aab5dcdfeb507af0582b673f84e4ae824e7
Lex Dingemans <a.dingemans@radboudumc.nl>
2022-10-12 08:59:19 -0400

    only 1 lime run is neede for test

--- phenoscore/tests/test_lime.py
+++ phenoscore/tests/test_lime.py
@@ -18,7 +18,7 @@ class CrossValLIMETester(unittest.TestCase):
         except:
             X, y, img_paths, df_data = self._phenoscorer.load_data_from_excel(os.path.join("phenoscore", "sample_data",
                                                                                        "satb1_data.xlsx"))
-        self._phenoscorer.get_lime(X, y, img_paths, n_lime=5)
+        self._phenoscorer.get_lime(X, y, img_paths, n_lime=1)
         self._phenoscorer.gen_lime_and_results_figure(bg_image=os.path.join("..", "sample_data",
                                                                             "background_image.jpg"),
                                                       df_data=df_data, filename='lime_figure_' + 'SATB1_test.pdf')
