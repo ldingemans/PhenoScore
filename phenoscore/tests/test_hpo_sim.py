b9b0624be6e428cd40a0a1af540061a413e47be0
Lex Dingemans <a.dingemans@radboudumc.nl>
2022-10-26 16:00:48 -0700

    fixed bug in test

--- phenoscore/tests/test_hpo_sim.py
+++ phenoscore/tests/test_hpo_sim.py
@@ -42,6 +42,6 @@ class SimScorerTester(unittest.TestCase):
         hpos = pd.DataFrame()
         hpos['hpo_all'] = ''
         hpos.at[0,'hpo_all'] = ['HP:0011927', 'HP:0000708', 'HP:0000709', 'HP:0008771', 'HP:0001250']
-        filtered_hpo = self.filter_hpo_df(hpos)
+        filtered_hpo = self._simscorer.filter_hpo_df(hpos)
         assert filtered_hpo.loc[0, 'hpo_all'] == ['HP:0001250']
 
