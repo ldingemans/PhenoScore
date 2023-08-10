d0cb648f664c1fc8176e2c6145c3791aa2cbecd2
Lex Dingemans <a.dingemans@radboudumc.nl>
2023-08-10 17:23:55 +0200

    added requirement for ``mxnet``

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", 'r') as fh:
     long_description = fh.read()
 
 setup(name='phenoscore',
-      version='1.0.1',
+      version='1.0.2',
       packages=find_packages(),
       install_requires=requirements,
 
