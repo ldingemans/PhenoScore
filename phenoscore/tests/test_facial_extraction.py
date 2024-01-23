a21dfd6b8c45ccaff206288292c34ae5d6fc71f1
Lex Dingemans <a.dingemans@radboudumc.nl>
2024-01-23 13:36:13 +0100

    Drop ``VGGFaceExtractor`` and ``tensorflow`` and ``deepface`` dependencies.

--- phenoscore/tests/test_facial_extraction.py
+++ phenoscore/tests/test_facial_extraction.py
@@ -1,5 +1,5 @@
 import unittest
-from phenoscore.facial_feature_extraction.extract_facial_features import VGGFaceExtractor, QMagFaceExtractor
+from phenoscore.facial_feature_extraction.extract_facial_features import QMagFaceExtractor
 import pandas as pd
 import numpy as np
 import os
@@ -12,7 +12,6 @@ class FacialFeatureExtractionTester(unittest.TestCase):
         path_to_script.insert(1, os.sep)
         path_to_qmagface = os.path.join(*path_to_script, 'facial_feature_extraction')
         self._qmagface = QMagFaceExtractor(path_to_dir=path_to_qmagface)
-        self._vggface = VGGFaceExtractor()
 
     def test_positive_output(self):
         path_to_script = os.path.realpath(__file__).split(os.sep)[:-2]
@@ -20,10 +19,7 @@ class FacialFeatureExtractionTester(unittest.TestCase):
         path_to_img = os.path.join(*path_to_script, 'sample_data', 'control_1.png')
         assert(type(self._qmagface.process_file(path_to_img)) == list)
         assert(len(self._qmagface.process_file(path_to_img)) == 512)
-        assert (type(self._vggface.process_file(path_to_img)) == list)
-        assert (len(self._vggface.process_file(path_to_img)) == 2622)
 
     def test_negative_output(self):
         random_img = (np.random.standard_normal([28, 28, 3]) * 255).astype(np.uint8)
         assert(self._qmagface.process_file(random_img) is None)
-        assert(self._vggface.process_file(random_img) is None)
