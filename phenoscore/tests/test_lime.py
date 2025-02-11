98c1121026afc2fe42a727ead25bc0c6d5ba3a0b
Lex Dingemans <69890037+ldingemans@users.noreply.github.com>
2025-02-11 16:45:05 +0100

    Move generation of vus figure to tutorial test.

--- phenoscore/tests/test_lime.py
+++ phenoscore/tests/test_lime.py
@@ -3,26 +3,14 @@ from phenoscore.phenoscorer import PhenoScorer
 import os
 
 
-# WARNING: this test does a full run and takes a while to run on a CPU
-
-
 class CrossValLIMETester(unittest.TestCase):
     def setUp(self):
         self._phenoscorer = PhenoScorer(gene_name='SATB1', mode='both')
 
     def test_lime_gen(self):
-        try:
-            X, y, img_paths, df_data = self._phenoscorer.load_data_from_excel(os.path.join("..", "sample_data",
-                                                                                       "satb1_data.xlsx"))
-        except:
-            X, y, img_paths, df_data = self._phenoscorer.load_data_from_excel(os.path.join("phenoscore", "sample_data",
-                                                                                       "satb1_data.xlsx"))
+        path_to_script = os.path.realpath(__file__).split(os.sep)[:-2]
+        path_to_script.insert(1, os.sep)
+        X, y, img_paths, df_data = self._phenoscorer.load_data_from_excel(os.path.join(*path_to_script, "sample_data",
+                                                                                   "satb1_data.xlsx"))
         self._phenoscorer.get_lime(X, y, img_paths, n_lime=1)
-        try:
-            self._phenoscorer.gen_lime_and_results_figure(bg_image=os.path.join("..", "sample_data",
-                                                                                "background_image.jpg"),
-                                                          df_data=df_data, filename='lime_figure_' + 'SATB1_test.pdf')
-        except:
-            self._phenoscorer.gen_lime_and_results_figure(bg_image=os.path.join("phenoscore", "sample_data",
-                                                                                "background_image.jpg"),
-                                                          df_data=df_data, filename='lime_figure_' + 'SATB1_test.pdf')
+        assert self._phenoscorer.lime_results is not None
